3a65973b19a2e03a5fef60e2c8942b776340fb67
Daniel <danielabad@gmail.com>
2024-03-22 18:14:29 -0400

    Creating Scope

---
+++ src/semantics/scope.py
@@ -0,0 +1,11 @@
+class Scope:
+    def __init__(self, parent = None, declarated_vars = {}) -> None:
+        self.local_vars = declarated_vars
+        self.parent:Scope = parent
+
+    def newContext(self, varset):
+        return Scope(self, varset)
+    
+    def IsDefined(self, var):
+        if var in self.local_vars: return True
+        else: return False if self.parent is None else self.parent.IsDefined(var)
