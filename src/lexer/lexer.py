7eed1afc52ac2081f3c9ad29dcfaf2d411661ef7
Anabel Benítez <anabelbenitezglez@gmail.com>
2024-03-22 13:53:24 -0400

    Refactor lexer

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,28 +1,32 @@
-from src.utils import Token
 from src.automaton import State
-from src.pycompiler import Terminal
 from src.regex.regex_automaton import get_regex_automaton
+from src.utils import Token
 
-def get_lexer(regexs: list[tuple[str, Terminal]]):
-    inicial = State(None)
 
-    for i, value in enumerate(regexs):
-        regex, terminal = value
-        
-        si, sf = get_regex_automaton(regex)
-        
-        sf.state = (i, terminal)
+class Lexer:
+    def __init__(self, table, eof):
+        self.eof = eof
+        self.automaton = self._build_automaton(table)
 
-        inicial.add_epsilon_transition(si)
-    
-    return inicial
+    @staticmethod
+    def _build_automaton(table):
+        start = State(None)
 
-def get_tokens(inicial : State, text : str):
-        tokens = []
+        for i, value in enumerate(table):
+            regex, terminal = value
 
+            si, sf = get_regex_automaton(regex)
+
+            sf.state = (i, terminal)
+
+            start.add_epsilon_transition(si)
+
+        return start
+
+    def _tokenize(self, text):
         max_pos = (0, None)
 
-        current_states = inicial.epsilon_closure
+        current_states = self.automaton.epsilon_closure
         i = 0
 
         while i < len(text):
@@ -33,7 +37,6 @@ def get_tokens(inicial : State, text : str):
                 if char in state.transitions:
                     for new_state in state.transitions[char]:
                         new_states.update(new_state.epsilon_closure)
-
 
             current_states = new_states
 
@@ -52,16 +55,19 @@ def get_tokens(inicial : State, text : str):
                             priority = state.state[0]
 
             if (len(current_states) == 0 or i + 1 == len(text)) and max_pos[1] is not None:
-                tokens.append(Token(text[max_pos[0] : max_pos[1] + 1], max_pos[2]))
+                yield text[max_pos[0]: max_pos[1] + 1], max_pos[2]
                 i = max_pos[1]
                 max_pos = (max_pos[1] + 1, None)
-                current_states = inicial.epsilon_closure
+                current_states = self.automaton.epsilon_closure
 
-            elif (len(current_states) == 0 or i + 1 == len(text)):
+            elif len(current_states) == 0 or i + 1 == len(text):
                 i = max_pos[0]
                 max_pos = (max_pos[0] + 1, None)
-                current_states = inicial.epsilon_closure
+                current_states = self.automaton.epsilon_closure
 
             i += 1
 
-        return tokens
+        yield '$', self.eof
+
+    def __call__(self, text):
+        return [Token(lex, ttype) for lex, ttype in self._tokenize(text) if ttype is not None]
