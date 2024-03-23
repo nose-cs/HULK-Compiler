2638c852858e77eff47a1d1f6dbc401378390698
Daniel <danielabad@gmail.com>
2024-03-23 03:03:42 -0400

    improving first visit

--- src/semantics/get_types_visitor.py
+++ src/semantics/get_types_visitor.py
@@ -1,10 +1,32 @@
 import src.hulk_grammar.hulk_ast_nodes as hulk_nodes
 import src.visitor as visitor
+from global_Context import GlobalContext 
+
+class Types_Visitor(object):
 
 
-def visit(node: hulk_nodes.ProgramNode):
-    types = {}
-    for decl in node.declarations:
-        if(decl is hulk_nodes.TypeDeclarationNode):
-            types[decl.idx] = len(decl)
+    def __init__(self) -> None:
+        self.context = GlobalContext()
+        self.errorsList = []
 
+    @visitor.on('node')
+    def visit(self, node):
+        pass
+
+    @visitor.when(hulk_nodes.ProgramNode)
+    def visit(self, node: hulk_nodes.ProgramNode):
+        for decl in node.declarations:
+            self.visit(decl)
+        return self.context , self.errorsList 
+
+    @visitor.when(hulk_nodes.TypeDeclarationNode)
+    def visit(self, node: hulk_nodes.TypeDeclarationNode):
+        self.errorsList.append(self.context.define_type(node))
+
+    @visitor.when(hulk_nodes.FunctionDeclarationNode)
+    def visit(self, node: hulk_nodes.FunctionDeclarationNode):
+        self.errorsList.append(self.context.define_function(node))
+
+    @visitor.when(hulk_nodes.ProtocolDeclaration)
+    def visit(self, node: hulk_nodes.ProtocolDeclaration):
+        self.errorsList.append(self.context.ed)
