a0a5109b7390d130c25aca74e08d57bea3b1f804
Daniel <danielabad@gmail.com>
2024-03-24 02:29:01 -0400

    Adding default types

--- src/semantics/type.py
+++ src/semantics/type.py
@@ -1,4 +1,4 @@
-class type:
+class Type:
     def __init__(self, id) -> None:
         self.id = id
         self.args_names = []
@@ -7,4 +7,19 @@ class type:
         self.propierties_types = []
         self.methods = {}
 
-    
+
+class StringType(Type):
+    def __init__(self) -> None:
+        super().__init__('String')
+
+class BoolType(Type):
+    def __init__(self) -> None:
+        super().__init__('Bool')
+
+class NumberType(Type):
+    def __init__(self) -> None:
+        super().__init__('Number')
+
+class ObjectType(Type):
+    def __init__(self, id) -> None:
+        super().__init__('Object')        
